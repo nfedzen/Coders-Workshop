8336061fe8eb92e2daf369c897f6250facdb5eba
Nathan Fedzen <nfedzen@hotmail.com>
2021-04-27 17:03:56 -0600

    completed addAndCheck Challenge

--- Coding-Challenges/addAndCheck/addAndCheck.js
+++ Coding-Challenges/addAndCheck/addAndCheck.js
@@ -1,1 +1,35 @@
 #!/usr/bin/env node
+class Object {
+  constructor(){
+    this.box = []
+  }
+
+  get(){
+    return this.box
+  }
+
+  add(value) {
+    this.box.push(value)
+  }
+
+  check(value) {
+    let found = false
+    this.box.forEach(number => {
+      if(number === value){
+        found = true
+      }
+    })
+    return found
+  }
+}
+
+
+let obj = new Object
+
+obj.add(10)
+obj.add(11)
+obj.add(12)
+
+console.log(obj.get())
+console.log(obj.check(10))
+console.log(obj.check(13))

4414b9f1912946ea16ed63e95c873f4eaefa1bb7
Nathan Fedzen <nfedzen@hotmail.com>
2021-04-29 16:35:17 -0600

    added debugged

--- Coding-Challenges/alternatingLinkedList/alternatingLinkedList.js
+++ Coding-Challenges/alternatingLinkedList/alternatingLinkedList.js
@@ -24,8 +24,41 @@ class linkedList {
     this.head = head
   }
 
+  printList(){
+    let print = this.head
+    while(print !== null){
+      console.log(print.data)
+      print = print.next
+    }
+  }
+
   arrange(){
-    let start = this.head
+    let beginning = this.head
+    let current = null
+    let next = beginning.next
+    let swap = 0
+    if(beginning.data > next.data){
+      swap = beginning.data
+      beginning.data = next.data
+      next.data = swap
+      current = next
+      next = current.next
+    } else {
+      current = next
+      next = current.next
+    }
+    while(next !== null){
+      if(current.data > next.data){
+        swap = current.data
+        current.data = next.data
+        next.data = swap
+        current = next
+        next = current.next
+      } else {
+        current = next
+        next = current.next
+      }
+    }
 
   }
 }
@@ -39,7 +72,9 @@ let node5 = new Node(5)
 
 node1.setNextNode(node2)
 node2.setNextNode(node3)
-node3.setNextNode(node2)
-node1.setNextNode(node2)
+node3.setNextNode(node4)
+node4.setNextNode(node5)
+let linkedList1 = new linkedList(node1)
+linkedList1.arrange()
+linkedList1.printList()
 
-console.log(node1.getNextNodeData())

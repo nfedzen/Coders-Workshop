4151c2851d1b38b4d68a93df29b70de0dc6e1f4d
Nathan Fedzen <nfedzen@hotmail.com>
2021-05-03 11:53:29 -0600

    finished coinFlip challenge

---
+++ Coding-Challenges/autocomplete/autocomplete.js
@@ -0,0 +1,11 @@
+
+const autocomplete = (string, set) => {
+let complete = set.forEach(word => {
+  let splitWord = word.split('')
+  let splitString = string.split('')
+  counter = 0;
+  while (splitWord.length() > counter){
+    
+  }
+});
+}
--- Coding-Challenges/coinFlip/coinFlip.js
+++ Coding-Challenges/coinFlip/coinFlip.js
@@ -1,7 +1,13 @@
 #!/usr/bin/env node
 
 const coinFlip = n => {
-
+  counter = 0
+  while(n > 1){
+    n = n/2
+    counter++
+  }
+  return counter
 };
 
-console.log(coinFlip(412)); // 9
+
+console.log(coinFlip(190892)); // 9
